291fcf1ee305b34340ff4ba4d8cc99df3f8af966
Manuel Durán <mata.duran@gmail.com>
2025-07-14 15:50:45 -0600

    Update sync.py vsdfsdf

--- sync.py
+++ sync.py
@@ -1,6 +1,8 @@
 from google.auth.impersonated_credentials import IDTokenCredentials
+import google.auth
 import google.auth.transport.requests
-
+from google.auth import credentials
+from google.oauth2 import service_account
 def generate_auth_token(audience: str, target_principal: str):
     """Generate Auth token needed to call API hosted on Cloud Run
     Args:
@@ -9,14 +11,23 @@ def generate_auth_token(audience: str, target_principal: str):
     Returns:
     Auth Identity token
     """
+    print(audience)
+    print(target_principal)
     scopes = ['https://www.googleapis.com/auth/cloud-platform']
+    source_credentials1 = (
+    service_account.Credentials.from_service_account_file(
+        '${{ secrets.GCP_SA_KEY }}',
+        scopes=scopes))
     request = google.auth.transport.requests.Request()
     creds, _ = google.auth.default(scopes=scopes)
     icreds = google.auth.impersonated_credentials.Credentials(
-        source_credentials=creds,
+        source_credentials=source_credentials1,
         target_principal=target_principal,
         target_scopes=scopes)
     id = IDTokenCredentials(icreds, target_audience=audience,include_email=True)
     id.refresh(request)
+    print(id.token)
     return id.token
+
+token = generate_auth_token("test","test112024@manuelmata-dev.iam.gserviceaccount.com")
 
