dd06bb032c79da4a242618ceca6679ae75027393
Manuel Durán <mata.duran@gmail.com>
2025-07-16 16:07:43 -0600

    Update sync.py

--- sync.py
+++ sync.py
@@ -3,7 +3,7 @@ import google.auth
 import google.auth.transport.requests
 from google.auth import credentials
 from google.oauth2 import service_account
-import os
+
 def generate_auth_token(audience: str, target_principal: str):
     """Generate Auth token needed to call API hosted on Cloud Run
     Args:
@@ -14,22 +14,18 @@ def generate_auth_token(audience: str, target_principal: str):
     """
     print(audience)
     print(target_principal)
-    print( os.environ['TOKEN'])
+ 
     scopes = ['https://www.googleapis.com/auth/cloud-platform']
-    source_credentials1 = (
-    service_account.Credentials.from_service_account_file(
-        '${{ secrets.GCP_SA_KEY }}',
-        scopes=scopes))
     request = google.auth.transport.requests.Request()
     creds, _ = google.auth.default(scopes=scopes)
     icreds = google.auth.impersonated_credentials.Credentials(
-        source_credentials=source_credentials1,
+        source_credentials=creds,
         target_principal=target_principal,
         target_scopes=scopes)
     id = IDTokenCredentials(icreds, target_audience=audience,include_email=True)
     id.refresh(request)
     print(id.token)
     return id.token
-
-token = generate_auth_token("test","test112024@manuelmata-dev.iam.gserviceaccount.com")
-
+audience="test"
+sa1="firebase-app-hosting-compute@manuelmata-dev.iam.gserviceaccount.com"
+generate_auth_token(audience,sa1)
