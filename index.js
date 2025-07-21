27a9510171ad8aa0bc7f9a1e3cee612a036a288c
Manuel Durán <mata.duran@gmail.com>
2025-07-21 11:59:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World w2';
+  const name = process.env.NAME || 'World w23';
   res.send(`Hello ${name}!`);
 });
 
